83579697587fbcb2449520e2837bdfe19a2ab67c
bala-amypo <bala@amypo.in>
2025-12-19 08:15:50 +0000

    User commit

--- src/main/java/com/example/demo/entity/University.java
+++ src/main/java/com/example/demo/entity/University.java
@@ -1,1 +1,25 @@
-65
+package com.example.demo.entity;
+
+
+import jakarta.persistence.*;
+
+
+@Entity
+public class University {
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+private Long id;
+private String name;
+private String country;
+private Boolean active = true;
+
+
+public Long getId() { return id; }
+public void setId(Long id) { this.id = id; }
+public String getName() { return name; }
+public void setName(String name) { this.name = name; }
+public String getCountry() { return country; }
+public void setCountry(String country) { this.country = country; }
+public Boolean getActive() { return active; }
+public void setActive(Boolean active) { this.active = active; }
+}
