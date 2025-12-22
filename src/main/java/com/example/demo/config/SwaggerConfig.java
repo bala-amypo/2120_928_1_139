470123bdc0af843d2430bd9b101bc55fed856ae9
bala-amypo <bala@amypo.in>
2025-12-22 09:02:35 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -20,7 +20,7 @@ public class SwaggerConfig {
                         .description("CRUD APIs for University Course Transfer System")
                 )
                 .servers(List.of(
-                        new Server().url("https://9495.pro604cr.amypo.ai/")
+                        new Server().url("https://9265.pro604cr.amypo.ai/")
                 ));
     }
 }
