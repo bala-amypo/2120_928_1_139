919d834d1d6958ec6bbade20e235563931028d21
bala-amypo <bala@amypo.in>
2025-12-27 07:39:06 +0000

    User commit

---
+++ src/test/com/example/demo/TestResultListener.java
@@ -0,0 +1,23 @@
+
+package com.example.demo;
+
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+public class TestResultListener implements ITestListener {
+    
+    @Override
+    public void onTestStart(ITestResult result) {
+        System.out.println("Test starting: " + result.getName());
+    }
+    
+    @Override
+    public void onTestSuccess(ITestResult result) {
+        System.out.println("Test passed: " + result.getName());
+    }
+    
+    @Override
+    public void onTestFailure(ITestResult result) {
+        System.out.println("Test failed: " + result.getName());
+    }
+}
